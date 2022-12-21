5119a583449670f83052fcac857e7da7f52dd008
Rishabh Verma <2000rishabhverma@gmail.com>
2022-12-22 00:45:47 +0530

    added shuffle parameter to api

--- routes/discover.js
+++ routes/discover.js
@@ -3,10 +3,19 @@ const category = require('../../cgip-micro-1-main/models/category');
 const router = express.Router();
 const imgGallery = require("../models/imgGallery");
 
-router.get("/get-images/:category", async (req, res, next) => {
+let skip = 0;
+router.get("/get-images/:category/:shuffle", async (req, res, next) => {
 	/* route to get 4 categories of images */
 	const reqCategory = req.params.category;
-	const getImages = await imgGallery.find({ category: { $in: [reqCategory] } }).select({ name: 1, _id: 0 }).sort({ createdAt: 0 }).limit(4);
+	const shuffle = req.params.shuffle;
+
+	
+	skip = (skip + parseInt(shuffle)); // it will give new images everytime we click shuffle.
+
+	const getImages = await imgGallery.find({ category: { $in: [reqCategory] } }).select({ name: 1, _id: 0 }).sort({ createdAt: 1 }).skip(skip).limit(2);
+	if(getImages.length===0){
+		skip = 0;
+	}
 	res.json(getImages);
 	/* sending json file of the requested categories */
 });
@@ -18,6 +27,5 @@ router.get("/get-images", async (req, res, next) => {
 	res.json(getImages);
 	/* sending json file of the requested category */
 });
-
 
 module.exports = router;
